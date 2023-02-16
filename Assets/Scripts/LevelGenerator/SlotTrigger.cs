bb73cdb47838a52c6d3d7394ef60113aa77797e1
Denis <zvoyder@protonmail.com>
2023-02-16 19:05:53 +0100

    fixing slottrigger

--- Assets/Scripts/LevelGenerator/SlotTrigger.cs
+++ Assets/Scripts/LevelGenerator/SlotTrigger.cs
@@ -5,17 +5,13 @@ using UnityEngine;
 public class SlotTrigger : MonoBehaviour
 {
     public string triggerTag = Constants.Tags.PLAYER;
-    public float drag = 5f;
-    public float centerPrecision = 0.2f;
-
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag(triggerTag))
         {
             other.transform.parent = transform.parent;
-            StartCoroutine(CenterInSlot(other.transform));
-        }   
+        }
     }
 
     private void OnTriggerExit(Collider other)
@@ -24,21 +20,5 @@ public class SlotTrigger : MonoBehaviour
         {
             other.transform.parent = null;
         }
-    }
-
-    private IEnumerator CenterInSlot(Transform toThrow)
-    {
-        while (
-            !Extension.Methods.Mathematics.Approximately(toThrow.transform.position.x, transform.position.x, centerPrecision)
-            || !Extension.Methods.Mathematics.Approximately(toThrow.transform.position.z, transform.position.z, centerPrecision)) //position check
-        {
-
-            toThrow.transform.position = Vector3.Lerp(toThrow.transform.position,
-                     new Vector3(transform.position.x, toThrow.transform.position.y, transform.position.z), drag * Time.fixedDeltaTime); //Set position based only on the Y
-
-            yield return new WaitForFixedUpdate();
-        }
-
-        yield return null;
     }
 }
